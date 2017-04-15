ea48642f2598f7c9e8151e925c1d52fa97726dea
liuxingpu <547137554@qq.com>
2017-04-15 14:51:49 +0800

    修改模版、完善文章编辑和提交

--- blog/controllers/main.py
+++ blog/controllers/main.py
@@ -2,13 +2,15 @@
 
 # from blog import app
 from flask import render_template,Blueprint
-from flask import current_app
+from flask import current_app,request
 from flask_login import current_user
 
 from ..forms.auth import SignForm
 from ..utils.db import blogdb
 
 main = Blueprint("main", __name__)
+
+user_id = '12345678'
 
 @main.route('/')
 def welcome():
@@ -39,6 +41,12 @@ def photo_album():
 def about_me():
     return render_template('about_me.html')
 
-@main.route('/edit_article')
+@main.route('/edit_article', methods=["GET", "POST"])
 def edit_article():
+    article = request.form.to_dict()
+    if article:
+        sql = """
+        insert into article (user_id,title,tags,content) VALUES (%s,%s ,%s ,%s)
+        """
+        blogdb.execute(sql,user_id,article['title'],article['tags'],article['content'])
     return render_template('edit_article.html')
